f76ba6edc38a714d1f4edfa38907b84c87692dec
Anurag <rawatanurag362@gmail.com>
2023-02-08 18:07:37 +0530

    Second Commit

--- src/Component/navBar.js
+++ src/Component/navBar.js
@@ -14,29 +14,29 @@ export default function NavBar(props) {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Smile <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nev-item">
+            <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
-            <li className="nev-item">
+            <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 About Us
               </Link>
             </li>
-            <li className="nev-item">
+            <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Services
               </Link>
             </li>
-            <li className="nev-item">
+            <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Contact Us
               </Link>
